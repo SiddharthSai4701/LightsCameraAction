716d7ca5ea904d1fedf9889d67374cc1db919a2f
Siddharth Vijay Sai <sidvsai@gmail.com>
2026-01-24 14:12:39 +0530

    Added movie model

---
+++ models/movie.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+/**
+ * Definethe schema for the movie resource to be stored in the db
+ */
+
+const movieSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    cast: {
+        type: [String],
+        required: true
+    },
+    trailerUrl: {
+        type: String,
+        required: true
+    },
+    language: {
+        type: [String],
+        required: true,
+        default: "English"
+    },
+    releaseDate: {
+        type: String,
+        required: true
+    },
+    director: {
+        type: String,
+        required: true
+    },
+    releaseStatus: {
+        type: String,
+        required: true,
+        default: "RELEASED"
+    },
+}, {
+    timestamps: true
+})
+
+const Movie = mongoose.model('Movie', movieSchema);
+module.exports = Movie;
